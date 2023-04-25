2b10903cd252614e79c39766899a4a5f3d3d0375
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-04-26 01:43:17 +0800

    color picker (mostly) done

--- src/components/colorPanel.svelte
+++ src/components/colorPanel.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import ColorPicker from './colorPicker.svelte';
+	let hue = 0;
+	let saturation = 100;
+	let lightness = 50;
 </script>
 
-<div class='p-4'>
-<ColorPicker />
+<div class="p-4 picker-container">
+	<ColorPicker bind:hue bind:saturation {lightness} />
+
+	<input
+		type="range"
+		min="0"
+		max="100"
+		style={`background: linear-gradient(to right, hsl(${hue.toFixed(0)}, ${saturation.toFixed(
+			0
+		)}%, 0%), hsl(${hue.toFixed(0)}, ${saturation.toFixed(0)}%, 100%))`}
+		bind:value={lightness}
+	/>
+
+  <div class="w-16 h-16" style={`background-color: hsl(${hue}, ${saturation}%, ${lightness}%)`} /> 
 </div>
+
+<style lang="postcss">
+	.picker-container {
+		--picker-background: theme('colors.base-200');
+	}
+</style>
--- src/components/colorPicker.svelte
+++ src/components/colorPicker.svelte
@@ -1,111 +1,76 @@
 <script lang="ts">
 	import { colord } from 'colord';
-	import { onMount } from 'svelte';
-
-	let pickerPalette: HTMLDivElement;
-
-	let hue = 0;
-	let saturation = 100;
-	let lightness = 50;
-
-	let cursorX = 0;
-	let cursorY = 0;
 
 	let mouseDown = false;
-	let parentX = 0;
-	let parentY = 0;
+	let picker: HTMLDivElement;
 
-	onMount(() => {
-		const parent = pickerPalette?.parentElement;
-		parentX = parent?.offsetLeft || 0;
-		parentY = parent?.offsetTop || 0;
-	});
-	function getComputedXY() {
-		return {
-			x: saturation * 1.8 * Math.cos((hue * Math.PI) / 180) + 180,
-			y: -180 - saturation * 1.8 * Math.sin((hue * Math.PI) / 180)
-		};
-	}
+	export let hue = 0;
+	export let saturation = 100;
+	export let lightness = 50;
 
-	function recalculateHS(e: MouseEvent) {
-		// radius/center-relative coordinates
-		const rx = e.offsetX - 180;
-		const ry = 180 - e.offsetY;
+	const setPos = (x: number, y: number) => {
+		if (!picker.parentElement) return;
+		const left = picker.offsetLeft;
+		const top = picker.offsetTop;
+		hue = Math.min(Math.max(x - left, 0), 360);
+		saturation = Math.min(Math.max((y - top) / 2, 0), 100);
 
-		hue =
-			ry > 0
-				? (Math.atan2(ry, rx) * 180) / Math.PI
-				: ((Math.atan2(ry, rx) + Math.PI * 2) * 180) / Math.PI;
-		saturation = Math.min(Math.max(Math.sqrt(rx * rx + ry * ry) / 1.8, 0), 100);
-		console.log({ rx, ry, hue, saturation });
-	}
+		console.log({ left, top, x, y, hue, saturation });
+	};
 
-	function recomputeXY() {
-		let coords = getComputedXY();
-
-		cursorX = coords.x;
-		cursorY = coords.y;
-	}
-
-	function onMouseDown(e: MouseEvent) {
+	const onMouseDown = (e: MouseEvent) => {
 		mouseDown = true;
-		recalculateHS(e);
-		recomputeXY();
-	}
-
-	function onMouseMove(e: MouseEvent) {
 		if (mouseDown) {
-			recalculateHS(e);
-			recomputeXY();
+			setPos(e.x, e.y);
 		}
-	}
-	function onMouseUp() {
+	};
+
+	const onMouseUp = () => {
 		mouseDown = false;
-	}
+	};
+
+	const onMouseMove = (e: MouseEvent) => {
+		if (mouseDown) {
+			setPos(e.x, e.y);
+		}
+	};
+
+	$: bgStyle = `
+		background: linear-gradient(
+				to right,
+				hsl(0, 100%, ${lightness}%),
+				hsl(60, 100%, ${lightness}%),
+				hsl(120, 100%, ${lightness}%),
+				hsl(180, 100%, ${lightness}%),
+				hsl(240, 100%, ${lightness}%),
+				hsl(300, 100%, ${lightness}%),
+				hsl(360, 100%, ${lightness}%)
+			),
+			linear-gradient(to bottom, hsl(0, 0%, ${lightness}%), hsl(0, 100%, ${lightness}%));
+	`;
 </script>
 
-<div class="color-picker-bg">
-	<div
-		class="color-picker"
-		bind:this={pickerPalette}
-		on:mouseup={onMouseUp}
-		on:mousemove={onMouseMove}
-		on:mousedown={onMouseDown}
-	>
-		<div class="relative top-0 left-0" style="font-size: 1000px; font-weight: 100;">
-			<svg width="360" height="360">
-				<line x1="0" y1="180" x2="360" y2="180" stroke="black" stroke-width="2" />
-				<line x1="180" y1="0" x2="180" y2="360" stroke="black" stroke-width="2" />
-			</svg>
-		</div>
-		<div class="cursor" style={`left: ${cursorX - 7}px; top: ${cursorY -7}px;`} />
-	</div>
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
+
+<div
+	class="color-picker"
+	style={bgStyle}
+	bind:this={picker}
+	on:mouseup={onMouseUp}
+	on:mousemove={onMouseMove}
+	on:mousedown={onMouseDown}
+>
+	<div class="cursor" style={`left: ${hue - 7}px; top: ${2 * saturation - 7}px;`} />
 </div>
+
+<div style="--lightness:{lightness}" />
 
 <style>
 	.color-picker {
 		width: 360px;
-		height: 360px;
-		border-radius: 50%;
-		transform: rotate(-90deg);
-	}
+		height: 200px;
 
-	.color-picker-bg {
-		width: 360px;
-		height: 360px;
-		border-radius: 50%;
-		background: conic-gradient(
-				hsl(0, 100%, 50%),
-				hsl(60, 100%, 50%),
-				hsl(120, 100%, 50%),
-				hsl(180, 100%, 50%),
-				hsl(240, 100%, 50%),
-				hsl(300, 100%, 50%),
-				hsl(360, 100%, 50%)
-			),
-			radial-gradient(hsl(0, 0%, 50%), hsl(0, 100%, 50%));
 		background-blend-mode: hue;
-		transform: rotate(90deg);
 	}
 
 	.cursor {
