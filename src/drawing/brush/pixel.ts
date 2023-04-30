1f2e2aef5548e9bdb90e7950e106a45812098402
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-04-30 22:27:38 +0800

    remove unneeded `console.log`

--- src/drawing/brush/pixel.ts
+++ src/drawing/brush/pixel.ts
@@ -3,7 +3,6 @@ import Brush from './brush';
 const PixelBrush = new Brush({
   name: 'Pixel Brush',
   fn: (frame, color, x, y) => {
-    console.log({frame, color, x, y})
     frame.data[4 * (y * frame.width + x)] = color.r;
     frame.data[4 * (y * frame.width + x) + 1] = color.g;
     frame.data[4 * (y * frame.width + x) + 2] = color.b;
