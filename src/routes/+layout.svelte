e68426cd483134c3596e9753e70f662745f24554
IWillRekU3206 <37181282+iwillreku3206@users.noreply.github.com>
2023-05-02 00:12:06 +0800

    populate top bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,8 +11,8 @@
 				children: [
 					{
 						label: 'New',
-            isLabel: true,
-            labelFor: 'new-file-modal'
+						isLabel: true,
+						labelFor: 'new-file-modal'
 					},
 					{
 						label: 'Recent...',
@@ -32,7 +32,58 @@
 						]
 					}
 				]
-			}
+			},
+			{
+				label: 'Edit',
+				children: [
+					{
+						label: 'Undo',
+						action: () => {
+							console.log('Undo');
+						}
+					},
+					{
+						label: 'Redo',
+						action: () => {
+							console.log('Redo');
+						}
+					}
+				]
+			},
+			{
+				label: 'View',
+				children: [
+					{
+						label: 'Zoom In',
+						action: () => {
+							console.log('Zoom In');
+						}
+					},
+					{
+						label: 'Zoom Out',
+						action: () => {
+							console.log('Zoom Out');
+						}
+					},
+					{
+						label: 'Reset Zoom',
+						action: () => {
+							console.log('Reset Zoom');
+						}
+					}
+				]
+			},
+      {
+        label: 'Help',
+        children: [
+          {
+            label: 'About',
+            action: () => {
+              console.log('About');
+            }
+          }
+        ]
+      }
 		]}
 	/>
 	<slot />
